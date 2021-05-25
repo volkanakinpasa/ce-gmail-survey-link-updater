405bd5fbaf86e04e8585a8f8bef962085f400d3a
Volkan Akin <Volkan.Akin@holidaycheck.com>
2021-05-25 13:46:13 +0200

    update logo & icon & remove unused npm

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -35,14 +35,15 @@ const Popup = () => {
     }
   }, []);
 
-  // const startContent = () => {
-  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  //     chrome.tabs.sendMessage(tabs[0].id, { type: 'start' });
-  //   });
-  // };
   return (
     <>
-      <div>
+      <div className='container'>
+        <div className='logo-container'>
+          <img
+            className='logo'
+            src={chrome.runtime.getURL('survey-stance-logo.png')}
+          />
+        </div>
         <fieldset>
           <legend>Options</legend>
           <div className='pb-10'>
